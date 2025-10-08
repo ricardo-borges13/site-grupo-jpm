a77d0c37c85d4885450608953e6b09349699012d
ricardo-borges13 <ricardo.borges@ymail.com>
2025-10-07 22:54:14 -0300

    primeiro commit. Subir projeto para o github

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { GlobalStyle } from "./assets/style/global"
+
+function App() {
+
+  return (
+    <>
+      <GlobalStyle />
+      <div className="global-container">
+        ola
+      </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/style/global.ts
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from "styled-components";
+import { normalize } from "styled-normalize";
+
+export const GlobalStyle = createGlobalStyle`
+    ${normalize}
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+       body {
+        background-color: #F9FAFB;        
+        width: 100%;
+        max-width: 100%;
+        margin: 0;
+        font-family: "Inter", "Roboto", sans-serif;
+        font-size: 16px;      
+        color: #1A1A1A;  
+    }
+
+    .global-container {
+      width: 100%;
+      max-width: clamp(320px, 90vw, 1200px);
+      margin: 0 auto;
+      padding: 0 16px;
+}
+
+  `

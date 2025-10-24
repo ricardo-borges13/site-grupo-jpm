e487b9e7d558c2b53f27b2729d848959546220c2
ricardo-borges13 <ricardo.borges@ymail.com>
2025-10-23 22:33:32 -0300

    informação de como foi feito o version

--- src/version.ts
+++ src/version.ts
@@ -3,3 +3,11 @@ import packageJson from '../package.json';
 export const APP_VERSION = packageJson.version;
 
 console.info(`Grupo JPM - Versão ${APP_VERSION} - Build ${new Date().toLocaleString('pt-BR')}`);
+
+//tsconfig.app.json
+//Inserido as seguintes linhas dentro de "compilerOptions": "resolveJsonModule": true, "esModuleInterop": true
+//No Footer.tsx: import { APP_VERSION } from '../../version';
+// <small>Versão {APP_VERSION}</small>
+//Desta forma, quando for alterar a versão basta mudar no arquivo package.json
+
+

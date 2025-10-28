4e4f130ef06d9133754eb0664d856e7369234f0a
ricardo-borges13 <ricardo.borges@ymail.com>
2025-10-28 14:17:49 -0300

    alterado grid BorrachasIndustriais

--- src/pages/BorrachasIndustriais/BorrachasIndustriais.styles.ts
+++ src/pages/BorrachasIndustriais/BorrachasIndustriais.styles.ts
@@ -49,7 +49,7 @@ export const Container = styled.div`
 
 export const ContainerProduct = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
   gap: 2rem;
   margin: 4.5rem 0 7.5rem 0;
 `;
